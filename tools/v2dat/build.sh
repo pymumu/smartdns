5710bdb2ab807c0e9099aff6dd18a61259a78839
weiping <liuweiping@msn.com>
2025-03-13 13:56:30 +0800

    remove tools

--- tools/v2dat/build.sh
+++
@@ -1,3 +0,0 @@
-
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o v2dat -trimpath -ldflags -s -w -buildid= .
-CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -o v2dat-arm -trimpath -ldflags "-s -w -buildid=" .
--- tools/v2dat/cmd/root.go
+++
@@ -1,9 +0,0 @@
-package cmd
-
-import (
-	"github.com/spf13/cobra"
-)
-
-var RootCmd = &cobra.Command{
-	Use: "v2dat",
-}
