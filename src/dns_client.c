9b296bfeb137ddf9395e2fe84101c00f37ca1928
HiGarfield <HiGarfield@126.com>
2025-03-27 00:05:02 +0800

    dns_client: fix compatibility with openssl 1.0

--- src/dns_client.c
+++ src/dns_client.c
@@ -332,7 +332,12 @@ static ssize_t _ssl_write_ext2(struct dns_server_info *server, SSL *ssl, const v
 #ifdef OSSL_QUIC1_VERSION
 	ret = SSL_write_ex2(ssl, buff, num, flags, &written);
 #else
+#if OPENSSL_VERSION_NUMBER >= 0x10101000L && !defined(OPENSSL_NO_SSL_WRITE_EX)
 	ret = SSL_write_ex(ssl, buff, num, &written);
+#else
+	ret = SSL_write(ssl, buff, num);
+	written = (ret > 0) ? (size_t)ret : 0;
+#endif
 #endif
 	pthread_mutex_unlock(&server->lock);
 
